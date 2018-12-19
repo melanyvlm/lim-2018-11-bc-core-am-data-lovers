8c17b0b3c16dfb63927d4c684061520ffa311d72
itafi <itafi@hotmail.com>
2018-12-19 16:47:11 -0500

    pantalla inicio con el card enlazado

--- src/data.js
+++ src/data.js
@@ -1,19 +1,30 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
+// esta es una función de ejemplo
+// puedes ver como agregamos la función a nuestro objeto global window
+
+/*const example = () => {
+  return 'example';
+};*/
 
 window.POKEMON = POKEMON;
 
 let ingresoValor = parseInt(document.getElementById("ingreso-km").value);
 
- function filtrar (ingresoValor) {
-  
-if (ingresoValor === 2 ) {
-return POKEMON.pokemon.filter(recorrido => (recorrido.egg === "2 km")).map(nom => `${nom.name}`);
+function filtrar(ingresoValor) {
+  let prueba = "";
+  if (ingresoValor === 2) {
+    prueba = POKEMON.pokemon.filter(recorrido => recorrido.egg === "2 km");
+    //console.log(prueba);
+    return prueba;
+  }
+  if (ingresoValor === 5) {
+    prueba = POKEMON.pokemon.filter(recorrido => (recorrido.egg === "5 km"));
+    return prueba;
+  }
+
+  if (ingresoValor === 10) {
+    prueba = POKEMON.pokemon.filter(recorrido => (recorrido.egg === "10 km"));
+    return prueba;
+  }
 }
- if (ingresoValor === 5 ) {
-return POKEMON.pokemon.filter(recorrido => (recorrido.egg === "5 km")).map(nom => `${nom.name}`);
-}
- if (ingresoValor === 10 ) {
-  return POKEMON.pokemon.filter(recorrido => (recorrido.egg === "10 km")).map(nom => `${nom.name}`);
- } 
-};
