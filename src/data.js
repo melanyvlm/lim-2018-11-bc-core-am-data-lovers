58239803a59d3e7eefb7fc6636ae81682ac9cc18
melanyvlm <mel.valencia28@outlook.com>
2018-12-20 17:00:49 -0500

    Cambios en menu , filtrado de huevos

--- src/data.js
+++ src/data.js
@@ -3,28 +3,27 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-/*const example = () => {
+/* const example = () => {
   return 'example';
 };*/
 
 window.POKEMON = POKEMON;
 
-let ingresoValor = parseInt(document.getElementById("ingreso-km").value);
-
+let ingresoValor = parseInt(document.getElementById('ingreso-km').value);
 function filtrar(ingresoValor) {
   let prueba = "";
   if (ingresoValor === 2) {
-    prueba = POKEMON.pokemon.filter(recorrido => recorrido.egg === "2 km");
-    //console.log(prueba);
+    prueba = POKEMON.pokemon.filter(recorrido => recorrido.egg === '2 km');
+    
     return prueba;
   }
   if (ingresoValor === 5) {
-    prueba = POKEMON.pokemon.filter(recorrido => (recorrido.egg === "5 km"));
+    prueba = POKEMON.pokemon.filter(recorrido => (recorrido.egg === '5 km'));
     return prueba;
   }
 
   if (ingresoValor === 10) {
-    prueba = POKEMON.pokemon.filter(recorrido => (recorrido.egg === "10 km"));
+    prueba = POKEMON.pokemon.filter(recorrido => (recorrido.egg === '10 km'));
     return prueba;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,68 +1,72 @@
 
-//mostrando pantalla de inicio
+// mostrando pantalla de inicio
 
-document.getElementById("pantalla-inicio").style.display="block";
-document.getElementById("pantalla-dos").style.display="none";
-document.getElementById("pantalla-tres").style.display="none";
-document.getElementById("pantalla-cuatro").style.display="none";
-
-const btnHistoria= document.getElementById("historia");
-const btnEquipos= document.getElementById("equipos");
-const btnPokemon= document.getElementById("pokemon");
-const btnInicio=document.getElementById("inicio");
-
-btnHistoria.addEventListener("click",irHistoria);
-btnEquipos.addEventListener("click",irEquipos);
-btnPokemon.addEventListener("click",irPokemon);
-btnInicio.addEventListener("click",irInicio);
+document.getElementById('pantalla-inicio').style.display = 'block';
+document.getElementById('pantalla-dos').style.display = 'none';
+document.getElementById('pantalla-tres').style.display = 'none';
+document.getElementById('pantalla-cuatro').style.display = 'none';
+document.getElementById('pantalla-buscar-pokemon').style.display = 'none';
 
 
-function irHistoria (){
-document.getElementById("pantalla-inicio").style.display="none";
-document.getElementById("pantalla-dos").style.display="block";
-document.getElementById("pantalla-tres").style.display="none";
-document.getElementById("pantalla-cuatro").style.display="none";
+const btnHistoria = document.getElementById('historia');
+const btnEquipos = document.getElementById('equipos');
+const btnPokemon = document.getElementById('pokemon');
+const btnInicio = document.getElementById('inicio');
+const btnBuscarPokemon = document.getElementById('buscar');
+
+btnHistoria.addEventListener('click', irHistoria);
+btnEquipos.addEventListener('click', irEquipos);
+btnPokemon.addEventListener('click', irPokemon);
+btnInicio.addEventListener('click', irInicio);
+btnBuscarPokemon.addEventListener('click', buscarPokemon);
+function buscarPokemon() {
+  document.getElementById('pantalla-buscar-pokemon').style.display = 'block';
+  document.getElementById('pantalla-inicio').style.display = 'none';
+  document.getElementById('pantalla-dos').style.display = 'none';
+  document.getElementById('pantalla-tres').style.display = 'none';
+  document.getElementById('pantalla-cuatro').style.display = 'none';
 }
-
-function irEquipos (){
-document.getElementById("pantalla-inicio").style.display="none";
-document.getElementById("pantalla-dos").style.display="block";
-document.getElementById("pantalla-tres").style.display="none";
-document.getElementById("pantalla-cuatro").style.display="none";
+function irHistoria() {
+  document.getElementById('pantalla-inicio').style.display = 'none';
+  document.getElementById('pantalla-dos').style.display = 'block';
+  document.getElementById('pantalla-tres').style.display = 'none';
+  document.getElementById('pantalla-cuatro').style.display = 'none';
+  document.getElementById('pantalla-buscar-pokemon').style.display = 'none';
 }
+function irEquipos() {
+  document.getElementById('pantalla-inicio').style.display = 'none';
+  document.getElementById('pantalla-dos').style.display = 'block';
+  document.getElementById('pantalla-tres').style.display = 'none';
+  document.getElementById('pantalla-cuatro').style.display = 'none';
+  document.getElementById('pantalla-buscar-pokemon').style.display = 'none';
 
-function irPokemon (){
-document.getElementById("pantalla-inicio").style.display="none";
-document.getElementById("pantalla-dos").style.display="none";
-document.getElementById("pantalla-tres").style.display="none";
-document.getElementById("pantalla-cuatro").style.display="block";
 }
-
-function irInicio (){
-document.getElementById("pantalla-inicio").style.display="block";
-document.getElementById("pantalla-dos").style.display="none";
-document.getElementById("pantalla-tres").style.display="none";
-document.getElementById("pantalla-cuatro").style.display="none";
+function irPokemon() {
+  document.getElementById('pantalla-inicio').style.display = 'none';
+  document.getElementById('pantalla-dos').style.display = 'none';
+  document.getElementById('pantalla-tres').style.display = 'none';
+  document.getElementById('pantalla-cuatro').style.display = 'block';
+  document.getElementById('pantalla-buscar-pokemon').style.display = 'none';
 }
-
-const boton = document.getElementById("go");
-boton.addEventListener("click", manejadorClick);
-const containerList = document.getElementById ("container-list");
-
+function irInicio() {
+  document.getElementById('pantalla-inicio').style.display = 'block';
+  document.getElementById('pantalla-dos').style.display = 'none';
+  document.getElementById('pantalla-tres').style.display = 'none';
+  document.getElementById('pantalla-cuatro').style.display = 'none';
+  document.getElementById('pantalla-buscar-pokemon').style.display = 'none';
+}
+const boton = document.getElementById('go');
+boton.addEventListener('click', manejadorClick);
+const containerList = document.getElementById('container-list');
 function manejadorClick() {
-    let ingresoValor = parseInt(document.getElementById("ingreso-km").value);
-    const saveArrayFiltrar = filtrar(ingresoValor);
-    let saveHijoPapa = [];
-    for(let i=0; i<saveArrayFiltrar.length; i++){
-        saveHijoPapa.push( `<div class="grid-item">
-                                <input type = "image" src="${saveArrayFiltrar[i].img}">
-                                <label>${saveArrayFiltrar[i].name}</label>
-
-                            </div>
-                        `)
-
-    }
-    console.log('saveHijoPapa___= ' + saveHijoPapa);
-    
-    containerList.innerHTML = saveHijoPapa.join('');
+  let ingresoValor = parseInt(document.getElementById('ingreso-km').value);
+  const saveArrayFiltrar = filtrar(ingresoValor);
+  let saveHijoPapa = [];
+  for (let i = 0; i < saveArrayFiltrar.length; i++) {
+    saveHijoPapa.push(`<div class="grid-item">
+    <input type = "image" src="${saveArrayFiltrar[i].img}">
+     <label>${saveArrayFiltrar[i].name}</label>
+                       </div> `);
+  }
+  containerList.innerHTML = saveHijoPapa.join('');
 }
